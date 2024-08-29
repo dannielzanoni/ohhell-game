ac1555ebe91e8eb165f3f4ba6ced7038a1bcaebe
João Xavier <fastjonh@gmail.com>
2024-08-29 02:56:00 -0300

    retrieving token from localStorage and passing it in the auth header

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { AnonymousPlayer } from './lobby.service';
@@ -10,6 +10,15 @@ export class AuthService {
   constructor(private client: HttpClient) { }
 
   login(name: string, photoIndex: number) {
-    return this.client.post<AnonymousPlayer>(`${environment.api_url}/lobby`, { photo_index: photoIndex, nickname: name })
+    const url = `${environment.api_url}/auth/login`
+    const payload = { photo_index: photoIndex, nickname: name }
+
+    const token = localStorage.getItem('JWT_TOKEN');
+
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+
+    return this.client.post<AnonymousPlayer>(url, payload, { headers })
   }
 }
