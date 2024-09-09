b52e6d83b9ef018d390311da433bba226fbd4e64
João Xavier <fastjonh@gmail.com>
2024-09-09 00:46:19 -0300

    formatting

--- src/app/models/player.ts
+++ src/app/models/player.ts
@@ -1,51 +1,51 @@
 import { PlayerReadyDTO } from "../services/lobby.service";
 
 export type Player = {
-    type: "Anonymous",
-    data: AnonymousPlayer,
+  type: "Anonymous",
+  data: AnonymousPlayer,
 } | {
-    type: "Google",
-    data: GooglePlayer
+  type: "Google",
+  data: GooglePlayer
 };
 
 export type GooglePlayer = {
-    email: string;
-    picture: string;
-    name: string;
+  email: string;
+  picture: string;
+  name: string;
 }
 
 export type AnonymousPlayer = {
-    picture: string;
-    name: string;
-    id: string;
+  picture: string;
+  name: string;
+  id: string;
 }
 
 export type PlayerInfo = {
-    lifes: number;
-    data: Player;
-    ready: boolean;
-    setInfo: SetInfo | null;
-    bidding: boolean;
+  lifes: number;
+  data: Player;
+  ready: boolean;
+  setInfo: SetInfo | null;
+  bidding: boolean;
 }
 
 export type SetInfo = {
-    bid: number;
-    points: number;
+  bid: number;
+  points: number;
 }
 
 export function getPlayerId(player: Player) {
-    switch (player.type) {
-        case "Anonymous":
-            return player.data.id
-        case "Google":
-            return player.data.email
-        default:
-            throw new Error('Unknown player type');
-    }
+  switch (player.type) {
+    case "Anonymous":
+      return player.data.id
+    case "Google":
+      return player.data.email
+    default:
+      throw new Error('Unknown player type');
+  }
 }
 
 export function getPlayerInfo(player: Player) {
-    return { lifes: 5, data: player, ready: false, setInfo: null, bidding: false }
+  return { lifes: 5, data: player, ready: false, setInfo: null, bidding: false }
 }
 
 //id e pontos do jogador
