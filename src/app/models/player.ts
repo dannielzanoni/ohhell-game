9acca7828ec83e42a1afbc641d8a17b10bc56263
João Xavier <fastjonh@gmail.com>
2026-06-22 19:45:23 -0300

    fix: nickname on reconnect

--- src/app/models/player.ts
+++ src/app/models/player.ts
@@ -10,7 +10,7 @@ export type GooglePlayer = {
   email: string;
   picture: string;
   name: string;
-  data: { [key: string]: null }
+  data?: { [key: string]: null }
 }
 
 export type AnonymousPlayer = {
@@ -42,8 +42,30 @@ export function getPlayerId(player: Player) {
   }
 }
 
-export function getPlayerInfo(player: Player) {
-  return { lifes: 5, data: player, ready: false, setInfo: null, turnToPlay: false }
+export function getPlayerInfo(player: Player, ready = false): PlayerInfo {
+  return { lifes: 5, data: player, ready, setInfo: null, turnToPlay: false }
+}
+
+export function getPlayerNickname(player: Player) {
+  switch (player.type) {
+    case "Anonymous":
+      return player.data.data["nickname"] || player.data.id;
+    case "Google":
+      return player.data.name || player.data.email;
+    default:
+      throw new Error('Unknown player type');
+  }
+}
+
+export function getPlayerPicture(player: Player) {
+  switch (player.type) {
+    case "Anonymous":
+      return player.data.data["picture"] || '';
+    case "Google":
+      return player.data.picture || '';
+    default:
+      throw new Error('Unknown player type');
+  }
 }
 
 //id e pontos do jogador
